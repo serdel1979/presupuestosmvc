6d63148365351ed260668202d2859c3397df5e3e
Sergio De Luca <serg.r.deluca@gmail.com>
2023-12-15 18:36:22 -0300

    proyecto presupuestos dotnet mvc

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+
+        public IActionResult Crear()
+        {
+
+           
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [StringLength(maximumLength:50, MinimumLength =5, ErrorMessage = "La longitud debe ser de {2} a {1}")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un número")]
+        public int Orden { get; set; }
+    }
+}
